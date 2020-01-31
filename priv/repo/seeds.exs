442e3efbe69def7c38c2b5b7f9bc9c518d0e5792
togmund <togmund@gmail.com>
2020-01-31 15:44:24 -0800

    entries almost finished, need to guard against groups without many members

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -23,11 +23,12 @@ alias TheTab.Transactions.Receipt
 group_names = ["Golden Geese", "McTavish", "Bradford", "Woodwards", "Stronglot"]
 
 # People for Seeds
-person_names = ["Noah", "Ted", "Mike", "Gregg", "Mallory", "Kailey", "Tanyss", "Jon", "Tristan"]
+user_names = ["Noah", "Ted", "Mike", "Gregg", "Mallory", "Kailey", "Tanyss", "Jon", "Tristan"]
 
 # Possible Tags
 tags = ["Food", "Event", "Game"]
 
+# Create a group for each group_name
 group_names
 |> Enum.each(fn name ->
   Repo.insert!(%Group{
@@ -35,14 +36,16 @@ group_names
   })
 end)
 
-person_names
+# Create a user for each user_name
+user_names
 |> Enum.each(fn name ->
   Repo.insert!(%User{
     name: name
   })
 end)
 
-1..length(person_names)
+# Add each user to exactly one group
+1..length(user_names)
 |> Enum.each(fn index ->
   Repo.insert!(%Member{
     user_id: index,
@@ -50,6 +53,7 @@ end)
   })
 end)
 
+# Create a bunch of random receipts valued between $8 and $25
 1..800
 |> Enum.each(fn _index ->
   Repo.insert!(%Receipt{
@@ -58,17 +62,50 @@ end)
   })
 end)
 
+# Create a set of entries for each receipt
 TheTab.Transactions.list_receipts()
 |> Enum.each(fn receipt ->
+  # Picks a group at random
+  group =
+    1..length(group_names)
+    |> Enum.random()
+
+  # Gets the full member list
+  members =
+    group
+    |> TheTab.People.list_member_ids!()
+
+  # Picks one at random to be the payer
+  payer =
+    members
+    |> Enum.random()
+
+  # Creates the entry with the Payer
   %Entry{
     receipt_id: receipt.id,
-    member_id:
-      1..length(group_names)
-      |> Enum.random()
-      |> TheTab.People.list_member_ids!()
-      |> Enum.random(),
+    member_id: payer,
     amount_paid: receipt.total,
     share: true
   }
   |> Repo.insert!()
+
+  # Generates a list of members that doesn't include the payer
+  sharers =
+    members
+    |> Enum.filter(fn member -> member != payer end)
+    |> Enum.shuffle()
+    |> Enum.slice(
+      1..length(members)
+      |> Enum.random()
+    )
+    # Creates the entries for some random number of sharers, at least one
+    |> Enum.each(fn sharer ->
+      %Entry{
+        receipt_id: receipt.id,
+        member_id: sharer,
+        amount_paid: nil,
+        share: true
+      }
+      |> Repo.insert!()
+    end)
 end)
