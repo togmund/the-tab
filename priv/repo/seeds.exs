7fec79865b00344c00eb33c4618635b629b07f20
togmund <togmund@gmail.com>
2020-01-31 13:51:32 -0800

    scaffolding out seeds for testing

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,37 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+import Ecto.Query, warn: false
+alias TheTab.Repo
+
+alias TheTab.People.Group
+alias TheTab.People.Member
+alias TheTab.People.User
+
+alias TheTab.Transactions.Entry
+alias TheTab.Transactions.Receipt
+
+Repo.insert!(%Group{
+  name: "Something"
+})
+
+Repo.insert!(%User{
+  name: "Something"
+})
+
+Repo.insert!(%Member{
+  group_id: 1,
+  user_id: 1
+})
+
+Repo.insert!(%Receipt{
+  tag: "food",
+  total: 1
+})
+
+Repo.insert!(%Entry{
+  receipt_id: 1,
+  member_id: 1,
+  amount_paid: 1,
+  share: true
+})
